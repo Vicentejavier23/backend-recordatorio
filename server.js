d0ceba7a36626ed3c1aea6b4f719fdaf8bc2db3b
Vicentejavier23 <vi.bueno@duocuc.cl>
2026-05-08 20:52:52 -0400

    add variable

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const app = require("./src/app")
 const { iniciarScheduler } = require("./src/services/scheduler.service")
 const PORT = process.env.PORT || 3000
@@ -5,5 +6,7 @@ const PORT = process.env.PORT || 3000
 
 app.listen(PORT,()=>{
     console.log(`Servidor corriendo en puerto ${PORT}`)
+    console.log("HORA:", process.env.HORA_RECORDATORIO)
+    console.log("MINUTO:", process.env.MINUTO_RECORDATORIO)
     iniciarScheduler()
 })
